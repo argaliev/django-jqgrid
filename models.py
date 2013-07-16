9b4d3774f849426b319a88e113631eb4aea81325
argaliev <bootaniq@gmail.com>
2013-07-16 17:19:08 +0600

    Create models.py
    add history_entry table to store jqgrid parameters such as sorting, filters, colmodels for each user.

---
+++ models.py
@@ -0,0 +1,21 @@
+#coding: utf-8
+
+from django.db import models
+
+class user_table_prefer(models.Model):
+    user = models.ForeignKey('auth.User', verbose_name=u'Пользователь')
+    ptable = models.CharField(max_length=100, verbose_name=u'Таблица')
+    prefer = models.TextField(verbose_name=u'Настройки')
+    hidegrid = models.CharField(max_length=100, verbose_name=u'Скрыть/Показать')
+    filters = models.TextField(verbose_name=u'Фильтры')
+    colmodel = models.TextField(verbose_name=u'Параметры колонок')
+    sortname = models.CharField(max_length=100, verbose_name=u'Поле сортировки')
+    sortorder = models.CharField(max_length=100, verbose_name=u'Порядок сортировки')
+    colsorder = models.TextField(verbose_name=u'Порядок полей')
+
+    class Meta:
+        verbose_name = u'Пользовательские настройки таблиц'
+        verbose_name_plural = u'Пользовательские настройки таблиц'
+
+    def __unicode__(self):
+        return u"{0} {1}".format(unicode(self.user),unicode(self.ptable))
